0af02b39387da5970f90e1b979fa2c86e6cd3d2d
abislam <aislam360@gmail.com>
2019-09-12 09:18:26 -0400

    First commit

---
+++ Lab3.java
@@ -0,0 +1,9 @@
+//get 4 positive integer inputs from the user.
+//print largest and smallest number
+//print how many even vs odd numbers %
+//print how many are one digit vs two digit
+//sum of all even numbers
+//sum of all odd numbers
+//sum of all one digit numbers
+//sum of all two digit numbers
+//no arrays or loops allowed 
